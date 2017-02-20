412eef6725653b2ea63b580759612b4e517232d0
carbitello <alekzhu@yandex.ru>
2017-02-20 15:51:55 +0300

    debug tags

--- generator.js
+++ generator.js
@@ -11,8 +11,12 @@
             document.getElementById("exposure-mode").value = EXIF.getTag(this, "ExposureProgram");
             document.getElementById("focalLength").value = EXIF.getTag(this, "FocalLengthIn35mmFilm");
             document.getElementById("aperture").value = EXIF.getTag(this, "FNumber");
-            document.getElementById("iso").value = EXIF.getTag(this, "ISOSpeedRatings");
-
+            //document.getElementById("iso").value = EXIF.getTag(this, "ISOSpeedRatings");
+            //           
+            var allMetaData = EXIF.getAllTags(this);
+            var allMetaDataSpan = document.getElementById("allMetaDataSpan");
+            document.getElementById("iso").innerHTML = JSON.stringify(allMetaData, null, "\t");
+            //
             var reader = new FileReader();
             reader.onload = (function (theFile) {
                 return function (e) {
