fa4d14e0d8cea9c40feff00f5d815d342c8d5039
carbitello <alekzhu@yandex.ru>
2017-02-20 23:03:32 +0300

    images

--- options/settings.php
+++ options/settings.php
@@ -5,9 +5,9 @@
     </head>
     <body>
         <form id="options-manager" enctype="multipart/form-data" action="" method="POST">
-            <input type="button"  class="settings-button" id="camera-settings" name="camera-settings" value="Camera" /> 
-            <input type="button"  class="settings-button" id="lens-settings" name="lens-settings" value="Lens" />
-            <input type="button"  class="settings-button" id="tags-settings" name="tags-settings" value="Tags" />
+            <button class="settings-button" id="camera-settings"><img src="../images/camera.png" alt="" style="vertical-align:middle"></button>
+            <button class="settings-button" id="lens-settings"><img src="../images/lens.png" alt="" style="vertical-align:middle"></button>
+            <button class="settings-button" id="tags-settings"><img src="../images/htag.png" alt="" style="vertical-align:middle"></button>
             <input type="hidden" name="model" value="<?= $_POST['model']; ?>" />
             <input type="hidden" name="lens" value="<?= $_POST['lens']; ?>" />
         </form>
