1b5b1f7e0e72bc06a5995b7c0eedd0c40d5b4b88
carbitello <alekzhu@yandex.ru>
2017-02-22 22:04:15 +0300

    debug

--- generator/generator.php
+++ generator/generator.php
@@ -8,31 +8,33 @@
         $aperture = $_POST['aperture'];
         $iso = $_POST['iso'];
 
-        require_once("../modules/jsondb.php");
-        //$jdb  = new Jsondb('/DB/');
+        //echo $_SERVER['DOCUMENT_ROOT'].'/modules/jsondb.php';
+        require_once($_SERVER['DOCUMENT_ROOT'].'/modules/jsondb.php');
+        //require_once("../modules/jsondb.php");
+        $jdb  = new Jsondb('/DB/');
 
-        //$camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
-        //$lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
+        $camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
+        $lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
 
-        //$outputtext = 'Camera: '.$camera_data[0]['text'].','.PHP_EOL;
-        //$outputtext = $outputtext.'Lens: '.$lens_data[0]['text'].','.PHP_EOL;
+        $outputtext = 'Camera: '.$camera_data[0]['text'].','.PHP_EOL;
+        $outputtext = $outputtext.'Lens: '.$lens_data[0]['text'].','.PHP_EOL;
         $outputtext = $outputtext.'Exposure: '.$exposuremode.' '.$exposuretime.','.PHP_EOL;
         $outputtext = $outputtext.'Aperture: f/'.$aperture.','.PHP_EOL;
-        //if($lens_data[0]['needfl'] == 'TRUE') {
-        //    $outputtext = $outputtext.'Focal length: '.$focalLength.'mm,'.PHP_EOL;
-        //}
-        //$outputtext = $outputtext.'ISO: '.$iso.'.'.PHP_EOL;
+        if($lens_data[0]['needfl'] == 'TRUE') {
+            $outputtext = $outputtext.'Focal length: '.$focalLength.'mm,'.PHP_EOL;
+        }
+        $outputtext = $outputtext.'ISO: '.$iso.'.'.PHP_EOL;
 
-        //$categoriestext = '';
-        //foreach($camera_data[0]['htgroups'] as $category){
-        //    $categoriestext = $categoriestext.$category.PHP_EOL;
-        //}
-        //foreach($lens_data[0]['htgroups'] as $category){
-        //    $categoriestext = $categoriestext.$category.PHP_EOL;
-        //}
+        $categoriestext = '';
+        foreach($camera_data[0]['htgroups'] as $category){
+            $categoriestext = $categoriestext.$category.PHP_EOL;
+        }
+        foreach($lens_data[0]['htgroups'] as $category){
+            $categoriestext = $categoriestext.$category.PHP_EOL;
+        }
     }
      else {
-        //$outputtext = $_POST['outputtext'].generatecategoriestags(explode("\n", str_replace("\r", "", $_POST['categories'])));
+        $outputtext = $_POST['outputtext'].generatecategoriestags(explode("\n", str_replace("\r", "", $_POST['categories'])));
     }
     function get_shutter($shutter) {
         $result = ' sec.';
@@ -57,26 +59,27 @@
         };
         return $exposuremode;
     }
-    //function generatecategoriestags($categories) {
+    function generatecategoriestags($categories) {
     //    require_once("../modules/jsondb.php");
-    //    $jdb  = new Jsondb('/DB/');
-    //    $hts_data = $jdb->select('*', 'hts', '');
-    //    $result = '';
-    //    foreach($hts_data as $currenttag) {
-    //        $currenttagtext = $currenttag['tag'].' ';
-    //        if(strpos($result, $currenttagtext) === false) {
-    //            foreach($currenttag['groups'] as $currentgroup) {
-    //                if(in_array($currentgroup, $categories) && ($currentgroup != '')){
-    //                    $result = $result.$currenttag['tag'].' ';
-    //                }
-    //            }
-    //            if((count($currenttag['crossgroups']) > 1) && validcross($categories, $currenttag['crossgroups'])) {
-    //                $result = $result.$currenttag['tag'].' ';
-    //            }
-    //        }
-    //    }
-    //    return $result;
-    //}
+        require_once($_SERVER['DOCUMENT_ROOT'].'/modules/jsondb.php');
+        $jdb  = new Jsondb('/DB/');
+        $hts_data = $jdb->select('*', 'hts', '');
+        $result = '';
+        foreach($hts_data as $currenttag) {
+            $currenttagtext = $currenttag['tag'].' ';
+            if(strpos($result, $currenttagtext) === false) {
+                foreach($currenttag['groups'] as $currentgroup) {
+                    if(in_array($currentgroup, $categories) && ($currentgroup != '')){
+                        $result = $result.$currenttag['tag'].' ';
+                    }
+                }
+                if((count($currenttag['crossgroups']) > 1) && validcross($categories, $currenttag['crossgroups'])) {
+                    $result = $result.$currenttag['tag'].' ';
+                }
+            }
+        }
+        return $result;
+    }
     function validcross($categories, $crossgroups) {
         foreach($crossgroups as $currentcross) {
             if(($currentcross != '')) {
