c122f72434f72906b4215c9eca1f0bef33fa68da
carbitello <alekzhu@yandex.ru>
2017-02-22 21:49:10 +0300

    debug

--- generator/generator.php
+++ generator/generator.php
@@ -2,34 +2,34 @@
     //ini_set('display_errors',1);
     //error_reporting(E_ALL);
     if(empty($_POST['categories'])){
-        $exposuretime = get_shutter($_POST['exposure']);
-        $exposuremode = get_exposuretext($_POST['exposure-mode']);
-        $focalLength = $_POST['focalLength'];
-        $aperture = $_POST['aperture'];
-        $iso = $_POST['iso'];
+        //$exposuretime = get_shutter($_POST['exposure']);
+        //$exposuremode = get_exposuretext($_POST['exposure-mode']);
+        //$focalLength = $_POST['focalLength'];
+        //$aperture = $_POST['aperture'];
+        //$iso = $_POST['iso'];
 
-        require_once("../modules/jsondb.php");
-        $jdb  = new Jsondb('/DB/');
+        //require_once("../modules/jsondb.php");
+        //$jdb  = new Jsondb('/DB/');
 
-        $camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
-        $lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
+        //$camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
+        //$lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
 
-        $outputtext = 'Camera: '.$camera_data[0]['text'].','.PHP_EOL;
-        $outputtext = $outputtext.'Lens: '.$lens_data[0]['text'].','.PHP_EOL;
-        $outputtext = $outputtext.'Exposure: '.$exposuremode.' '.$exposuretime.','.PHP_EOL;
-        $outputtext = $outputtext.'Aperture: f/'.$aperture.','.PHP_EOL;
-        if($lens_data[0]['needfl'] == 'TRUE') {
-            $outputtext = $outputtext.'Focal length: '.$focalLength.'mm,'.PHP_EOL;
-        }
-        $outputtext = $outputtext.'ISO: '.$iso.'.'.PHP_EOL;
+        //$outputtext = 'Camera: '.$camera_data[0]['text'].','.PHP_EOL;
+        //$outputtext = $outputtext.'Lens: '.$lens_data[0]['text'].','.PHP_EOL;
+        //$outputtext = $outputtext.'Exposure: '.$exposuremode.' '.$exposuretime.','.PHP_EOL;
+        //$outputtext = $outputtext.'Aperture: f/'.$aperture.','.PHP_EOL;
+        //if($lens_data[0]['needfl'] == 'TRUE') {
+        //    $outputtext = $outputtext.'Focal length: '.$focalLength.'mm,'.PHP_EOL;
+        //}
+        //$outputtext = $outputtext.'ISO: '.$iso.'.'.PHP_EOL;
 
-        $categoriestext = '';
-        foreach($camera_data[0]['htgroups'] as $category){
-            $categoriestext = $categoriestext.$category.PHP_EOL;
-        }
-        foreach($lens_data[0]['htgroups'] as $category){
-            $categoriestext = $categoriestext.$category.PHP_EOL;
-        }
+        //$categoriestext = '';
+        //foreach($camera_data[0]['htgroups'] as $category){
+        //    $categoriestext = $categoriestext.$category.PHP_EOL;
+        //}
+        //foreach($lens_data[0]['htgroups'] as $category){
+        //    $categoriestext = $categoriestext.$category.PHP_EOL;
+        //}
     }
      else {
         //$outputtext = $_POST['outputtext'].generatecategoriestags(explode("\n", str_replace("\r", "", $_POST['categories'])));
