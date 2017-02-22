ffaf28d64f2c112d869491c8ef2d6f3d0078626c
Alexey Zhukov <alexey.zhukov@devexpress.com>
2017-02-22 17:05:44 +0300

    genrate text

--- generator/generator.php
+++ generator/generator.php
@@ -1,22 +1,37 @@
 <?php
-    $model = $_POST['model'];
-    $lens = $_POST['lens'];
-    $exposuretime = get_shutter($_POST['exposure']);
-    $exposuremode = $_POST['exposure-mode'];
-    $focalLength = $_POST['focalLength'];
-    $aperture = $_POST['aperture'];
-    $iso = $_POST['iso'];
+    if(empty($_POST['categories'])){
+        $exposuretime = get_shutter($_POST['exposure']);
+        $exposuremode = get_exposuretext($_POST['exposure-mode']);
+        $focalLength = $_POST['focalLength'];
+        $aperture = $_POST['aperture'];
+        $iso = $_POST['iso'];
 
-    require_once("../modules/jsondb.php");
-    $jdb  = new Jsondb('/DB/');
+        require_once("../modules/jsondb.php");
+        $jdb  = new Jsondb('/DB/');
 
-    $camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
-    $lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
+        $camera_data = $jdb->select('*', 'cams', Array('where'=>Array('name'=>$_POST['model']),));
+        $lens_data = $jdb->select('*', 'lens', Array('where'=>Array('id'=>$_POST['lens']),));
 
+        $outputtext = 'Camera: '.$camera_data[0]['text'].','.PHP_EOL;
+        $outputtext = $outputtext.'Lens: '.$lens_data[0]['text'].','.PHP_EOL;
+        $outputtext = $outputtext.'Exposure: '.$exposuremode.' '.$exposuretime.','.PHP_EOL;
+        $outputtext = $outputtext.'Aperture: f/'.$aperture.','.PHP_EOL;
+        if($lens_data[0]['needfl'] == 'TRUE')
+            $outputtext = $outputtext.'Focal length: '.$focalLength.'mm,'.PHP_EOL;
+        $outputtext = $outputtext.'ISO: '.$iso.'.'.PHP_EOL;
 
-    //echo '<pre>'.print_r($camera_data,1).'</pre>';
-    //echo '<pre>'.print_r($lens_data,1).'</pre>';
-
+        $categoriestext = '';
+        foreach($camera_data[0]['htgroups'] as $category){
+            $categoriestext = $categoriestext.$category.PHP_EOL;
+        }
+        foreach($lens_data[0]['htgroups'] as $category){
+            $categoriestext = $categoriestext.$category.PHP_EOL;
+        }
+        //echo '<pre>'.print_r($camera_data,1).'</pre>';
+        //echo '<pre>'.print_r($lens_data,1).'</pre>';
+    } else {
+        $outputtext = $_POST['outputtext'];    
+    }
     function get_shutter($shutter) {
         $result = ' sec.';
         if($shutter < 1) {
@@ -27,7 +42,22 @@
         }
         return $result;
     }
-
+    function get_exposuretext($exposuremode){
+        switch ( $exposuremode ) {
+            case 'Manual' :
+                return '#manualexposure';
+            case 'Normal program' :
+                return '#normalprogram';
+            case 'Aperture priority' :
+                return '#aperturepriority';
+            case 'Shutter priority' :
+                return '#shutterpriority';
+        };
+        return $exposuremode;
+    }
+    function generatecategoriestags($categories) {
+        
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,17 +67,9 @@
     </head>
     <body>
         <form method="post" action="generator.php">
-            <?php  
-                echo 'Camera: '.$model.',<br />';
-                echo 'Lens: '.$lens.',<br />';
-                echo 'Exposure: '.$exposuremode.' '.$exposuretime.',<br />';
-                echo 'Aperture: f/'.$aperture.',<br />';
-                echo 'Focal length: '.$focalLength.',<br />';
-                echo 'ISO: '.$iso.'.<br />';
-            ?>
-            <textarea name="categories"></textarea><br />
-            <textarea name="outputtext"></textarea><br />
-            <input type="submit" value="submit" />
+            <textarea name="categories"><?= $categoriestext; ?></textarea><br />
+            <textarea name="outputtext"><?= $outputtext; ?></textarea><br />
+            <input type="submit" value="generate" />
         </form>
     </body>
 </html>
